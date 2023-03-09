291c577bdddf6d421c25e9d42afdeb3c1c0c27ef
Serhii <grammas56@gmail.com>
2023-03-09 19:05:08 +0200

    JS_part_started

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+import tabs from './modules/tabs';
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+
+
+});
