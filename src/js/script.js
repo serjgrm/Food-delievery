0e26c20c1cef9f844cda61c2da943181a30f41ba
Serhii <grammas56@gmail.com>
2023-03-10 20:16:15 +0200

    slider_calc_js_part

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,22 @@
 import tabs from './modules/tabs';
-
+import calc from './modules/calc';
+import slider from './modules/slider';
 
 window.addEventListener('DOMContentLoaded', function() {
-    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+     
+    
 
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    calc();
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
 
 });
